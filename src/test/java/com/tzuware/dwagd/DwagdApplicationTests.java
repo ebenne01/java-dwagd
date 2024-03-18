6978819d1414d18f63d7f0e3df8e77f4fe5aa34b
Edward Bennett <44945949+ebenne01@users.noreply.github.com>
2024-03-18 11:18:23 -0400

    Initialize Spring Boot service

---
+++ src/test/java/com/tzuware/dwagd/DwagdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tzuware.dwagd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DwagdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
